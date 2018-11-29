5e01be5b31e4a9c0bd4eed3eaf002358cdba61f0
杨亚谕 <584219447@qq.com>
2018-11-29 14:50:00 +0800

    Initial commit

---
+++ microservicecloud-api/src/main/java/com/yang/springcloud/entities/Dept.java
@@ -0,0 +1,24 @@
+package com.yang.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@AllArgsConstructor//全参构造
+@NoArgsConstructor//无参构造
+@Data//set get
+@Accessors(chain = true)//链式返回
+public class Dept implements Serializable {
+    private Long  deptno;   //主键
+    private String  dname;   //部门名称
+    private String  db_source;//来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+
+    public Dept(String dname) {
+        super();
+        this.dname = dname;
+    }
+
+}
