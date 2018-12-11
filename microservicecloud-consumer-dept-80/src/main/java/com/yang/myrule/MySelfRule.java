4439b02764e34fe92154c3a6a6a79fff95060379
杨亚谕 <584219447@qq.com>
2018-12-11 21:31:00 +0800

    Ribbon自定义,轮询策略，但是加上新需求，每个服务器要求被调用5次

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/yang/myrule/MySelfRule.java
@@ -0,0 +1,15 @@
+package com.yang.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySelfRule {
+    @Bean
+    public IRule myRule() {
+      //  return new RandomRule();//Ribbon默认是轮询，我自定义为随机
+        return new RandomRule_ZY();//我自定义为每个机器被访问5次
+    }
+}
