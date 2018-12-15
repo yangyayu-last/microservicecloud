4e332f03fbf6b4bc20cda159cf8779811e782c0b
杨亚谕 <584219447@qq.com>
2018-12-15 14:57:02 +0800

    新建feign客户端，api微服务新增feign的接口，Feign是一个声明式的Web服务客户端，Feign集成了Ribbon

---
+++ microservicecloud-api/src/main/java/com/yang/springcloud/service/DeptClientService.java
@@ -0,0 +1,21 @@
+package com.yang.springcloud.service;
+
+import java.util.List;
+
+import com.yang.springcloud.entities.Dept;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+public interface DeptClientService {
+    @RequestMapping(value = "/dept/get/{id}",method = RequestMethod.GET)
+    public Dept get(@PathVariable("id") long id);
+
+    @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
+    public List<Dept> list();
+
+    @RequestMapping(value = "/dept/add",method = RequestMethod.POST)
+    public boolean add(Dept dept);
+}
