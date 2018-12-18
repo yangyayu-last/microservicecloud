ed6c9d9568d7a00f126491db83170f57b8b0ee6f
杨亚谕 <584219447@qq.com>
2018-12-18 22:36:00 +0800

    Hystrix服务降级

--- microservicecloud-api/src/main/java/com/yang/springcloud/service/DeptClientService.java
+++ microservicecloud-api/src/main/java/com/yang/springcloud/service/DeptClientService.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+//@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
 public interface DeptClientService {
     @RequestMapping(value = "/dept/get/{id}",method = RequestMethod.GET)
     public Dept get(@PathVariable("id") long id);
